10ed08bde4a042aec2b58a44ac7be7fdbd80113b
mmed <noemail@gmail.com>
2023-07-18 15:26:12 +0100

    completed research

--- core.py
+++ core.py
@@ -5,6 +5,7 @@ import pandas as pd
 import numpy as np
 from pytesseract import Output
 import pytesseract as ts
+import cv2
 
 
 def get_image(file):
@@ -13,6 +14,7 @@ def get_image(file):
         image = full_image[0]
     else:
         image = Image.open(file)
+    image = np.array(image)
     return image
 
 
@@ -22,9 +24,10 @@ def isprice(text) -> bool:
     for i, c in enumerate(text):
         if (c == "$" or c == "€") and i == 0:
             curr = c
+            continue
         if c.isdigit():
             continue
-        if c == ".":
+        if c == "." or c ==",":
             if (
                 point == False
                 and i != 0
@@ -44,3 +47,25 @@ def isprice(text) -> bool:
         return False
     else:
         return point
+
+def is_name(word):
+    return not isprice(word) and not word.isdigit()
+
+def merge_names(line):
+    ind = 0
+    while ind < (len(line)-1) :
+        while ind+1 < len(line) and is_name(line[ind]) and is_name(line[ind+1]):
+            line[ind] += ' ' + line[ind+1]
+            line.pop(ind+1)
+        ind += 1
+
+def same_line(A,B):
+    if abs(A-B) < 10 :
+        return True 
+    else:
+        return False
+
+def clean_the_text(row):
+    row.text = "".join([c if ord(c) < 128 else "" for c in row.text]).strip()
+    row.text.replace("\n", "")
+    return row
