76a1e0495d7bd1eaa54443c07deaf6147a4402bc
mmed <noemail@gmail.com>
2023-07-29 14:20:20 +0100

    prepare for odoo integration

---
+++ odoo_integration/models/bill.py
@@ -0,0 +1,10 @@
+from odoo import models,fields
+from tkinter import filedialog
+
+
+class bill(models.Model):
+    _inherit = 'hr.expense'
+
+    def does_nothing(self):
+        file_path = self.env['ir.attachment'].with_context(binary_field='file_data').open_file_dialog()
+        print(file_path)
---
+++ production.py
@@ -0,0 +1,6 @@
+from core import *
+
+
+file = filedialog.askopenfilename()
+
+print(file)
