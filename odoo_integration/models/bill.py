7176cbe19d5d1880613834f35379087f18f198c7
mmed <noemail@gmail.com>
2023-08-01 09:39:54 +0100

    presentation

--- odoo_integration/models/bill.py
+++ odoo_integration/models/bill.py
@@ -5,6 +5,8 @@ from tkinter import filedialog
 class bill(models.Model):
     _inherit = 'hr.expense'
 
+    upload_file = fields.Binary(string='Upload file')
+    
     def does_nothing(self):
         file_path = self.env['ir.attachment'].with_context(binary_field='file_data').open_file_dialog()
         print(file_path)
